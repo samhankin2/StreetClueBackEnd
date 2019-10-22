433dc7c85b8b2a399e50d1668467ccec7bb70445
SamHankin <samhankin2>
2019-10-22 10:58:17 +0100

    done end of round and end of game

--- Main/Game.py
+++ Main/Game.py
@@ -1,25 +1,33 @@
 class Game:
     """Stores all the relvevant info for a single game to be played"""
 
-    def __init__(self, pin):
+    def __init__(self, pin, randomLocations, totalRounds):
         self.pin = pin
         self.arrayOfPlayers = []
         self.scores = {}
         self.round = 0
+        self.totalRounds = totalRounds
         self.playerCount = 0
-        # self.answerCount = 0
+        self.started = False
+        self.randomLocations = randomLocations
+        self.answerCount = 0
 
     def addPlayer(self, player):
         self.arrayOfPlayers.append(player)
         self.scores[player.name] = 0
 
-    def incRound(self):
-        self.round += 1
-        if(round == 5):
-            return self.pin
-
     def calcPlayerCount(self):
         self.playerCount = len(self.arrayOfPlayers)
 
     def updateScores(self, player, score):
+        self.answerCount += 1
         self.scores[player] += score
+
+    def startGame(self):
+        self.playerCount = len(self.arrayOfPlayers)
+        self.started = True
+        self.round += 1
+
+    def nextRound(self):
+        self.round += 1
+        return self.round
--- Main/app.py
+++ Main/app.py
@@ -2,14 +2,15 @@ from flask import Flask
 from flask import request, Response, jsonify
 import pusher
 from random import randrange, randint
+import random
 
 from Player import Player
 from Game import Game
 
 app = Flask(__name__)
 
+# TODO Make sure the names are unique
 
-# TODO Use Dictionary as the {pin:GameObject}
 
 channels_client = pusher.Pusher(
     app_id='882302',
@@ -21,7 +22,11 @@ channels_client = pusher.Pusher(
 
 
 takenPins = []
-
+locations = ["-37.2236053,145.929006",
+             "-42.7111515,146.8972924",
+             "41.2779077,146.036284",
+             "-44.5667837,170.198597",
+             "-38.6770894,176.07472470"]
 games = {}
 
 
@@ -37,26 +42,52 @@ def create_game():
 
     takenPins.append(pin)
 
-    newGame = Game(pin)
-    games[pin] = newGame
+    newGame = Game(pin, locations, 5)
 
+    games[pin] = newGame
     response = {"msg": "Created game sucessfully", "pin": pin}
     json = jsonify(response)
-    return json
+    return json, 201
 
 
-@app.route('/test', methods=['GET'])
+@app.route('/test', methods=['GET', 'POST'])
 def debug():
-    print(games)
-    for i in games:
-        print(games[i].pin)
-        print(games[i].arrayOfPlayers)
-        print(games[i].scores)
+    # print(games)
+    # for i in games:
+    #     print(games[i].pin)
+    #     print(games[i].arrayOfPlayers)
+    #     print(games[i].scores)
 
-    # print(availablePins)
-    # print(takenPins)
+    # # print(availablePins)
+    # # print(takenPins)
+
+    print(takenPins)
+    print(games)
+
+    newGame = Game("9999", locations, 3)
+    games["9999"] = newGame
+    newPlayer = Player("test")
+    newPlayer2 = Player("hello")
+    games["9999"].addPlayer(newPlayer)
+    games["9999"].addPlayer(newPlayer2)
+
+    games["9999"].startGame()
+
+    takenPins.append("9999")
 
     return "hi!"
+
+
+@app.route('/start_game', methods=['POST'])
+def start_game():
+    body = request.json
+    pin = body["pin"]
+
+    games[pin].startGame()
+
+    response = {"msg": "started game sucessfully", "pin": pin}
+    json = jsonify(response)
+    return json
 
 
 @app.route('/add_player', methods=['POST'])
@@ -70,15 +101,15 @@ def add_player():
         games[pin].addPlayer(newPlayer)
         channels_client.trigger(str(pin), 'playerJoin', {
                                 'message': playername + " Has Joined"})
-        response = {"msg": "Added "+playername+" Successfully"}
+
+        response = {"msg": "Added "+playername+" Successfully",
+                    "locations": games[pin].randomLocations}
         json = jsonify(response)
         return json, 201
     else:
         response = {"msg": pin + " Doesnt Exists"}
         json = jsonify(response)
         return json, 404
-
-    # TODO need to sort out the responses
 
 
 @app.route('/update_score', methods=['POST'])
@@ -87,14 +118,29 @@ def update_score():
     playername = body["name"]
     pin = body["pin"]
     score = body["score"]
+    print(takenPins)
 
     if pin in games:
         games[pin].updateScores(playername, score)
-        # TODO Pusher goes here for updating score realtime
-        return "Updated"
+        if games[pin].playerCount == games[pin].answerCount:
+            games[pin].answerCount = 0
+            games[pin].nextRound()
+            if games[pin].round > games[pin].totalRounds:
+                del games[pin]
+                takenPins.remove(pin)
+                return "End Game"
+            else:
+                response = {"msg": "End of Round",
+                            "scores": games[pin].scores}
+                json = jsonify(response)
+                return json, 200
+        else:
+            return "updated score"
 
     else:
-        return "No Game Exists"
+        response = {"msg": pin + " Doesnt Exists"}
+        json = jsonify(response)
+        return json, 404
 
 
 # @app.route('/next_round', methods=['POST'])
@@ -103,13 +149,19 @@ def update_score():
 #     # for
 
 
-# TODO sort out how we are negotiating the next round
-# TODO all pins needs to be strings or leading 0s wont work
-
-
 def generatePin():
     pin = ''.join(str(randint(0, 9)) for _ in range(4))
     return pin
+
+
+# def getLocations(numberOfRounds):
+#     randomLocations = []
+#     indexs = random.sample(range(0, len(locations)-1), numberOfRounds)
+
+#     for i in indexs:
+#         randomLocations.append(locations[i])
+
+#     return randomLocations
 
 
 def findGame(pin):
